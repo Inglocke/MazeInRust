99d5365235e09cdb36a64f15856871fe357da9a8
Martin Vigl <martinvigl06@gmail.com>
2024-02-10 15:03:01 +0100

    Maze Animation clean

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate minifb;
 
+use std::iter::FlatMap;
+
 use minifb::{Key, Window, WindowOptions};
 use rand::seq::SliceRandom;
 
@@ -65,18 +67,23 @@ fn main() {
     let mut window = Window::new("Rust Graphics", WIDTH, HEIGHT, WindowOptions::default())
         .unwrap_or_else(|e| panic!("{}", e));
 
+    let mut drawn = false;
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        draw_maze(&mut buffer, &spanning_tree, CELL_SIZE, WIDTH, HEIGHT, &mut window);
+        if !drawn{
+            drawn = draw_maze(&mut buffer, &spanning_tree, CELL_SIZE, WIDTH, HEIGHT, &mut window);
+        }
+        window.update();
+        //println!("{}", window.is_open());
     }
 }
 
 
-fn draw_maze(buffer: &mut Vec<u32>, spanning_tree: &[(Coord, Coord)], cell_size: usize, width: usize, height: usize, window: &mut Window) {
+fn draw_maze(buffer: &mut Vec<u32>, spanning_tree: &[(Coord, Coord)], cell_size: usize, width: usize, height: usize, window: &mut Window) -> bool{
     //clear_screen(buffer, 0x00_00_00_00);
     for edge in spanning_tree {
         if !window.is_open() || window.is_key_down(Key::Escape){
             println!("CLOSE");
-            return;
+            return true;
         }
         let x1 = edge.0.x * cell_size;
         let y1 = edge.0.y * cell_size;
@@ -85,7 +92,9 @@ fn draw_maze(buffer: &mut Vec<u32>, spanning_tree: &[(Coord, Coord)], cell_size:
         draw_line(buffer, width, height, x1, y1, x2, y2, 0xFF_FF_FF_FF);
         window.update_with_buffer(buffer, width, height).unwrap();
     }
+    true
 }
+
 
 
 fn draw_line(buffer: &mut Vec<u32>, width: usize, height: usize, mut x0: usize, mut y0: usize, x1: usize,  y1: usize, color: u32) {
